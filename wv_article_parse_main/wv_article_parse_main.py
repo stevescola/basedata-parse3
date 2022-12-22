0121dca2ffeddc25e0b26a74d55225923b71d738
stevescola <113270097+stevescola@users.noreply.github.com>
2022-12-22 15:47:36 +0800

    Create wv_article_parse_main.py

---
+++ wv_article_parse_main/wv_article_parse_main.py
@@ -0,0 +1,12 @@
+def wv_article_parse_main(article_bucket):
+    """
+    Orchestrates section file creation for each locale based on the article revision and keeps track of successfully processed articles and available section components
+    
+    Parameters
+    ----------
+    article_bucket: location of raw articles that will be parsed by this package
+    
+    """
+    
+    
+    return 
