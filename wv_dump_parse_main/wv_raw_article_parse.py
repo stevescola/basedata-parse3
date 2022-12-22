67b4bb65a6dbb2f754ad066744c76367d5c8352b
stevescola <113270097+stevescola@users.noreply.github.com>
2022-12-22 15:47:55 +0800

    Create wv_raw_article_parse.py

---
+++ wv_dump_parse_main/wv_raw_article_parse.py
@@ -0,0 +1,24 @@
+def raw_article_parse_json(...):
+    
+    """
+    Parse JSON dump into separate article files
+    Logic in: OneDrive/Disruptor/WikiVoyage/DumpPractice_original-PRACTICE.ipynb
+    
+    Parameters
+    ----------
+    ...
+        ...
+    
+    Return
+    ------
+    ...
+        Separate files for each locale article named as defined and in a folder with the article_id-revision_id naming
+    
+    """
+    
+  
+    wv_raw_article_parse_json()
+    ...
+    
+    
+    return ...
