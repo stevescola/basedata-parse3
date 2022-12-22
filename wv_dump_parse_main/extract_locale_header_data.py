cb40eb5ae14c22cff08a9ea84c12f86840f1a67d
stevescola <113270097+stevescola@users.noreply.github.com>
2022-12-22 15:53:31 +0800

    Update extract_locale_header_data.py

--- wv_dump_parse_main/extract_locale_header_data.py
+++ wv_dump_parse_main/extract_locale_header_data.py
@@ -1,17 +1,20 @@
-"""
-    Extracts specific tags from each "article" in the dump and creates a dictionary that describes the locale which is topic of the article.  
-    See the defined schema.
-    
-    Parameters
-    ----------
-    ...
-       ...
-       
-    Returns
-    -------
-    Single dictionary that includes a series of items that describe each locale
+def extract_locale_header_data(...):
+
+    """
+        Extracts specific tags from each "article" in the dump and creates a dictionary that describes the locale which is topic of the article.  
+        See the defined schema.
+        
+        Parameters
+        ----------
+        ...
+        ...
+        
+        Returns
+        -------
+        Single dictionary that includes a series of items that describe each locale
     """
 
-def extract_article_meta_data(...):
+...
 
 return ...
+
